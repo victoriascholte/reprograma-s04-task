b6fa6eccaacb821fd4a694b030f8abd4fe01a5f1
Victoria Scholte <victoria.scholte@gmail.com>
2022-04-22 17:58:28 -0300

    adjustments in tasks 04 and 05

--- task-05.js
+++ task-05.js
@@ -1,13 +1,14 @@
 // Fatorial
-// Crie uma função que irá receber um número e retornar o fatorial dele. por exemplo: 3! = 3 * 2 * 1 // 
+// Crie uma função que irá receber um número e 
+// retornar o fatorial dele. por exemplo: 3! = 3 * 2 * 1 // 
 
-
-
-// const fatorial = (numero) => {
-//     let multiplicacao = 1;
-
-//     for (let i = 1; i <= numero; i++) {
-//         multiplicacao = multiplicacao * i
-//         console.log(`${multiplicacao} = ${multiplicacao} * ${i}`)
-//     }
-// }
+function factorial(n) {
+    let factorial = n - 1;
+    let resultado = n * factorial;
+      while (factorial != 1){
+          factorial -= 1;
+          resultado *= factorial;
+      }
+      return resultado;
+}
+console.log(factorial(10))
